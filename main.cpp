32ba7dc51fe910b95b6f4cb71d61a14ddf881d0d
DamienOrl <damien.orlando@ynov.com>
2019-03-28 17:41:10 +0100

    DEL + humidité

--- main.cpp
+++ main.cpp
@@ -11,9 +11,13 @@ namespace {
 // Network interface
 NetworkInterface *net;
 
+static DigitalOut led1(LED1);
+static AnalogIn humidite(ADC_IN1);
 int arrivedcount = 0;
 const char* topic = "verner/feeds/message";
 const char* toptemp = "verner/feeds/temperature";
+const char* topled = "verner/feeds/led";
+const char* tophum =  "verner/feeds/humidite";
 I2C i2c(I2C1_SDA, I2C1_SCL);
 uint8_t lm75_adress = 0x48 << 1;
 
@@ -21,7 +25,16 @@ uint8_t lm75_adress = 0x48 << 1;
 void messageArrived(MQTT::MessageData& md)
 {
 	MQTT::Message &message = md.message;
-	printf("Message arrived: qos %d, retained %d, dup %d, packetid %d\r\n", message.qos, message.retained, message.dup, message.id);
+	if (message.payloadlen == 2) {
+		led1 = 1;
+	}
+	if (strcmp((const char *)message.payload, "ON") == 0) {
+		led1 = 1;
+	}
+	else if (strcmp((const char *)message.payload, "OFF") == 0) {
+		led1 = 0;
+	}
+
 	printf("Payload %.*s\r\n", message.payloadlen, (char*)message.payload);
 	++arrivedcount;
 }
@@ -69,7 +82,7 @@ int main() {
 
 	// Connect the MQTT Client
 	MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
-	data.MQTTVersion = 3;
+	data.MQTTVersion = 4;
 	data.clientID.cstring = "test";
 	data.username.cstring = "verner";
 	data.password.cstring = "c777cfc4b0f34f93b1045ac4512cbac4";
@@ -77,7 +90,7 @@ int main() {
 		printf("rc from MQTT connect is %d\r\n", rc);
 
 	// Subscribe to the same topic we will publish in
-	if ((rc = client.subscribe(topic, MQTT::QOS2, messageArrived)) != 0)
+	if ((rc = client.subscribe(topled, MQTT::QOS2, messageArrived)) != 0)
 		printf("rc from MQTT subscribe is %d\r\n", rc);
 
 
@@ -86,7 +99,7 @@ int main() {
 	MQTT::Message message;
 	// QoS 0
 	char buf[100];
-	sprintf(buf, "OSKOUUUUUR\r\n");
+	sprintf(buf, "It just works! Probably...\r\n");
 
 	message.qos = MQTT::QOS0;
 	message.retained = false;
@@ -96,35 +109,48 @@ int main() {
 
 	rc = client.publish(topic, message);
 
-// Send temperature:
+
+
+	// Send temperature + humidité:
 	while (true){
-			MQTT::Message messagetemp;
-			//rc = client.publish(topic, message);
-			char cmd[2];
-			cmd[0] = 0x00;
+		MQTT::Message messagetemp;
+		char cmd[2];
+		cmd[0] = 0x00;
 
-			i2c.write(lm75_adress, cmd, 1);
-			i2c.read(lm75_adress, cmd, 2);
+		i2c.write(lm75_adress, cmd, 1);
+		i2c.read(lm75_adress, cmd, 2);
 
-			float temperature = ((cmd[0] << 8 | cmd[1] ) >> 7) * 0.5;
-			printf("Temperature : %f\n", temperature);
+		float temperature = ((cmd[0] << 8 | cmd[1] ) >> 7) * 0.5;
+		printf("Temperature : %f\n", temperature);
 
-			//ThisThread::sleep_for(10000);
+		char buf2[100];
+		sprintf(buf2, "%f", temperature);
 
-			char buf2[100];
-			sprintf(buf2, "%f", temperature);
+		messagetemp.qos = MQTT::QOS0;
+		messagetemp.retained = false;
+		messagetemp.dup = false;
+		messagetemp.payload = (void*)buf2;
+		messagetemp.payloadlen = strlen(buf2)+1;
 
-			messagetemp.qos = MQTT::QOS0;
-			messagetemp.retained = false;
-			messagetemp.dup = false;
-			messagetemp.payload = (void*)buf2;
-			messagetemp.payloadlen = strlen(buf2)+1;
+		rc = client.publish(toptemp, messagetemp);
+		client.yield();
+		printf("RC %d\n", rc);
 
-			rc = client.publish(toptemp, messagetemp);
-			client.yield();
-			printf("RC %d\n", rc);
 
-			wait_ms(5000);
+		MQTT::Message messagehum;
+		float mesure = (humidite.read()*3.3) * 100 / 2.8;
+		char buf3[100];
+		printf("Humidité : %f\n", mesure);
+		sprintf(buf3,"%.2f",mesure);
+		messagehum.qos = MQTT::QOS0;
+		messagehum.retained = false;
+		messagehum.dup = false;
+		messagehum.payload = (void*)buf3;
+		messagehum.payloadlen = strlen(buf3);
+
+		rc = client.publish(tophum, messagehum);
+
+		wait_ms(3000);
 	}
 
 
